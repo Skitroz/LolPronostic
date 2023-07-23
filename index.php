52a89b9c0fea62821b87a1ef95546035f9c0820e
Skitroz <65279654+Skitroz@users.noreply.github.com>
2023-07-23 19:23:24 +0200

    Commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+    $titlePage = "LolKikoo - Accueil";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link href="output.css" rel="stylesheet">
+    <link href="src/css/index.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://kit.fontawesome.com/0d88df16c5.js" crossorigin="anonymous"></script>
+    <title><?php echo $titlePage;?></title>
+</head>
+<body class="background">
+    <header>
+        <menu>
+            <nav class="text py-6">
+                <ul class="pl-10 flex gap-10">
+                    <li>
+                        <a href="#" class="text-menu">Match</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu">Pronostic</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu">Jouer</a>
+                    </li>
+                    <li>
+                        <a href="#" class="text-menu">Classement</a>
+                    </li>    
+                    <li class="absolute top-2 right-0 mr-10">
+                        <a href="#" class="px-6 py-4 background-login"><i class="fa-regular fa-user"></i></a>
+                    </li>
+                </ul>
+            </nav>
+        </menu>
+    </header>
+    <main>
+        <div class="flex-col text-center pt-10">
+            <section>
+                <h1 class="text pb-16 text-[34px]">Résumé des matchs récents de la LEC</h1>
+                <?php
+
+$html = file_get_contents('https://www.team-aaa.com/fr/actualite/lec-summer-split-saison-2023-informations-streams-planning-resultats-classement_128106?page=2');
+
+// Créer un nouvel objet DOMDocument
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+
+// Rechercher tous les éléments avec la classe "versus-part"
+$versusElements = $dom->getElementsByTagName('div');
+
+// Tableau pour stocker les matchs
+$matches = [];
+
+// Parcourir les éléments "versus-part"
+foreach ($versusElements as $versusElement) {
+    // Vérifier si l'élément a la classe "versus-part"
+    if ($versusElement->getAttribute('class') === 'versus-part') {
+        // Récupérer le texte brut de l'élément
+        $text = $versusElement->textContent;
+
+        // Remplacer le score "1" par le score entouré de balises <span> avec la classe "text-red-500"
+        $text = preg_replace('/\b1\b/', '<span class="text-[#e0e0f1] bg-[#ff3b3b] py-2 px-3 rounded-[6px]">1</span>', $text);
+        $text = preg_replace('/\b0\b/', '<span class="text-[#e0e0f1] bg-[#b9b9b9] py-2 px-3 rounded-[6px]">0</span>', $text);
+
+        // Ajouter le texte extrait au tableau des matchs
+        $matches[] = $text;
+    }
+}
+
+// Grouper les matchs en 3 blocs de 5
+$groupedMatches = array_chunk($matches, 5);
+
+// Afficher les blocs de matchs en utilisant le système de grille de Tailwind
+echo '<div class="grid grid-cols-3 gap-4">';
+foreach ($groupedMatches as $group) {
+    echo '<div class="col-span-1">';
+    foreach ($group as $matchText) {
+        echo '<div class="p-4 m-2 bg-[#161729] rounded-[12px]">';
+        echo "<p class='text-[20px] text'>{$matchText}</p>";
+        echo '</div>';
+    }
+    echo '</div>';
+}
+echo '</div>';
+
+?>
+
+
+            </section>
+            <div class="flex justify-center gap-[250px] pt-10">
+                <section>
+                    <h2 class="text">Classement des équipes</h2>
+                </section>
+                <section>
+                    <h2 class="text">Classements des KikooJAPS</h2>
+                </section>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.php"],
+  theme: {
+    extend: {
+      colors: {
+        'text': '#fafafa',
+        'background': '#050505',
+        'primary': '#b7b462',
+        'secondary': '#273418',
+        'accent': '#b78962',
+      },
+    },
+  },
+  plugins: [],
+};
